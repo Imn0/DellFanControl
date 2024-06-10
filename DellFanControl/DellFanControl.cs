92256216a9d99ee99bbb021d28d49f54039557dd
imno <krzysztof.jan.andrzejewski@gmail.com>
2024-06-10 11:53:17 +0200

    cleaned up code

--- DellFanControl/DellFanControl.cs
+++ DellFanControl/DellFanControl.cs
@@ -1,90 +1,135 @@
 namespace DellFanControl;
 
 using System;
+using System.Timers;
 using System.Windows.Forms;
 using DellFanManagement.Interop;
+using Microsoft.Win32;
+
+enum FanState
+{
+    OFF,
+    ONE_SLOW,
+    SLOW,
+    FAST,
+}
 
 static class DellFanControl
 {
 
+    private static FanState currentFanState = FanState.OFF;
+    private static System.Timers.Timer? aTimer;
+
     static void SetOff()
     {
+        currentFanState = FanState.OFF;
         DellFanLib.DisableEcFanControl();
         DellFanLib.SetFanLevel(FanIndex.Fan2, FanLevel.Level0);
         DellFanLib.SetFanLevel(FanIndex.Fan1, FanLevel.Level0);
     }
     static void SetOneSlow()
     {
+        currentFanState = FanState.ONE_SLOW;
+
         DellFanLib.DisableEcFanControl();
         DellFanLib.SetFanLevel(FanIndex.Fan2, FanLevel.Level1);
         DellFanLib.SetFanLevel(FanIndex.Fan1, FanLevel.Level0);
     }
     static void SetSlow()
     {
+        currentFanState = FanState.SLOW;
+
         DellFanLib.DisableEcFanControl();
         DellFanLib.SetFanLevel(FanIndex.Fan2, FanLevel.Level1);
         DellFanLib.SetFanLevel(FanIndex.Fan1, FanLevel.Level1);
     }
-    static void SetFull()
+    static void SetFast()
     {
+        currentFanState = FanState.FAST;
+
         DellFanLib.DisableEcFanControl();
         DellFanLib.SetFanLevel(FanIndex.Fan2, FanLevel.Level2);
         DellFanLib.SetFanLevel(FanIndex.Fan1, FanLevel.Level2);
     }
 
+    static void TimerEvent(object? source, ElapsedEventArgs e){
+
+        switch (currentFanState)
+        {
+            case FanState.OFF:
+                SetOff();
+                break;
+            case FanState.ONE_SLOW:
+                SetOneSlow();
+                break;
+            case FanState.SLOW:
+                SetSlow();
+                break;
+            case FanState.FAST:
+                SetFast();
+                break;
+        }
+    }
+
     [STAThread]
     static void Main()
     {
-        try{
-        DellFanLib.Initialize();
-
-        SetOff();
-        ApplicationConfiguration.Initialize();
-
-        NotifyIcon notifyIcon = new()
+        try
         {
-            Icon = new Icon("./Poorly-drawn-fan.ico"),
-            Text = "Dell Fan Control",
-            Visible = true
-        };
-        ContextMenuStrip contextMenu = new();
-        List<ToolStripMenuItem> menuItems = [];
+            aTimer = new System.Timers.Timer(10000);
+            aTimer.Elapsed += new ElapsedEventHandler(TimerEvent);
+            aTimer.Enabled = true;
 
-        AddItem(contextMenu, menuItems, "Off", SetOff, ticked: true);
-        AddItem(contextMenu, menuItems, "One slow", SetOneSlow);
-        AddItem(contextMenu, menuItems, "Slow", SetSlow);
-        AddItem(contextMenu, menuItems, "Full", SetFull);
+            DellFanLib.Initialize();
 
+            SetOff();
+            ApplicationConfiguration.Initialize();
 
-        contextMenu.Items.Add(new ToolStripSeparator());
-        contextMenu.Items.Add("EXIT", null, (sender, args) => { Application.Exit(); });
-
-        notifyIcon.ContextMenuStrip = contextMenu;
-
-        notifyIcon.MouseClick += (sender, args) =>
-        {
-            if (args.Button == MouseButtons.Left)
+            NotifyIcon notifyIcon = new()
             {
-            }
-            else if (args.Button == MouseButtons.Right)
+                Icon = new Icon("./Poorly-drawn-fan.ico"),
+                Text = "Dell Fan Control",
+                Visible = true
+            };
+            ContextMenuStrip contextMenu = new();
+            List<ToolStripMenuItem> menuItems = [];
+
+            AddItem(contextMenu, menuItems, "Off", SetOff, ticked: true);
+            AddItem(contextMenu, menuItems, "One slow", SetOneSlow);
+            AddItem(contextMenu, menuItems, "Slow", SetSlow);
+            AddItem(contextMenu, menuItems, "Full", SetFast);
+
+
+            contextMenu.Items.Add(new ToolStripSeparator());
+            contextMenu.Items.Add("EXIT", null, (sender, args) => { Application.Exit(); });
+
+            notifyIcon.ContextMenuStrip = contextMenu;
+
+            notifyIcon.MouseClick += (sender, args) =>
             {
-            }
-        };
-        notifyIcon.MouseDoubleClick += (sender, args) =>
-        {
-            if (args.Button == MouseButtons.Right)
+                if (args.Button == MouseButtons.Left)
+                {
+                }
+                else if (args.Button == MouseButtons.Right)
+                {
+                }
+            };
+            notifyIcon.MouseDoubleClick += (sender, args) =>
             {
-                Application.Exit();
-            }
-        };
+                if (args.Button == MouseButtons.Right)
+                {
+                    Application.Exit();
+                }
+            };
             Application.Run();
-        }catch (Exception ex)
-    {
-        File.WriteAllText("error.log", ex.ToString());
-    }
+        }
+        catch (Exception ex)
+        {
+            File.WriteAllText("error.log", ex.ToString());
+        }
     }
     public delegate void VoidFunction();
-    static void AddItem(ContextMenuStrip contextMenu, List<ToolStripMenuItem> menuItems, String name, VoidFunction fanSetting, bool ticked=false)
+    static void AddItem(ContextMenuStrip contextMenu, List<ToolStripMenuItem> menuItems, string name, VoidFunction fanSetting, bool ticked = false)
     {
 
         ToolStripMenuItem fullItem = new(name, null, (sender, args) => { fanSetting(); })
